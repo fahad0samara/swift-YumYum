fd4348ce880d0078c097de52112ee576c26a15ae
fahad0samarar <fah8ad@outlook.com>
2024-03-01 18:45:33 +0200

    add some improve for Category

---
+++ YumYum/views/CategoryButton.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryButton.swift
+//  YumYum
+//
+//  Created by fahad samara on 3/1/24.
+//
+
+import SwiftUI
+
+struct CategoryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryButton()
+}
