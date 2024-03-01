86429a9f64b00710b8c6f2834b092ae176c6dd9c
fahad0samarar <fah8ad@outlook.com>
2024-03-01 07:21:52 +0200

    Initial Commit

---
+++ YumYum/YumYumApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumYumApp.swift
+//  YumYum
+//
+//  Created by fahad samara on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct YumYumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
