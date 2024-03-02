1215f6970617d9420e1f1181126170f03ecbcb1c
fahad0samarar <fah8ad@outlook.com>
2024-03-02 22:05:25 +0200

    updta the favorit and fishing

--- YumYum/viewmodel/FavoritesViewModel.swift
+++ YumYum/viewmodel/FavoritesViewModel.swift
@@ -11,12 +11,20 @@ class FavoritesViewModel: ObservableObject {
     @Published var favoriteItems: [FoodItem] = []
 
     // Add an item to favorites
-    func addToFavorites(_ foodItem: FoodItem) {
-        if !favoriteItems.contains(where: { $0.id == foodItem.id }) {
-            favoriteItems.append(foodItem)
-        }
-    }
 
+    // Add an item to favorites
+      func addToFavorites(_ foodItem: FoodItem) -> Bool {
+          if !favoriteItems.contains(where: { $0.id == foodItem.id }) {
+              favoriteItems.append(foodItem)
+              // Notify the view about the change
+              objectWillChange.send()
+              return true
+          } else {
+              // Item is already in favorites, handle this case (optional)
+              print("Item is already in favorites")
+              return false
+          }
+      }
     // Remove an item from favorites
     func removeFromFavorites(_ foodItem: FoodItem) {
         favoriteItems.removeAll(where: { $0.id == foodItem.id })
